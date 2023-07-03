9951f9262cdeca04e00e5ca7e533faa655931545
Ghazanfar Ali <ghazanfar.ali@azimutlabs.com>
2023-07-03 12:27:08 +0500

    service implemented for modal hide/show

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  templateUrl: './nav.component.html', 
+  styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
-
+export class NavComponent implements OnInit {
+  constructor(public modal: ModalService) {}
+  ngOnInit(): void {}
+  public openModal = ($event: Event) => {
+    $event.preventDefault();
+    this.modal.toggleModal();
+  };
 }
---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private visible: boolean = false;
+
+  constructor() {}
+  isModalOpen = () => {
+    return this.visible;
+  };
+  toggleModal = () => {
+    this.visible = !this.visible;
+  };
+}
