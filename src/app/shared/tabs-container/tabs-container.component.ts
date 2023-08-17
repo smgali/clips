b78643ea75d0b0d2bbd6e510683f81d2b598a5c2
Ghazanfar Ali <ghazanfar.ali@azimutlabs.com>
2023-08-17 13:04:45 +0500

    completed toggle switch in tabs component

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  OnInit,
+  AfterContentInit,
+  QueryList,
+} from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.css']
+  styleUrls: ['./tabs-container.component.css'],
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements OnInit, AfterContentInit {
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent> =
+    new QueryList();
 
+  /**
+   * NgOnInit doesn't work because the the tabs will be empty.
+   * Tabs will be initialized after the content init.
+   */
+  ngOnInit(): void {
+    console.log(this.tabs);
+  }
+
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach((tab) => (tab.active = false));
+    tab.active = true;
+    return false;
+  }
 }
